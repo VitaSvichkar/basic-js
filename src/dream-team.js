237c9900179f0767743e5f85f5b0b1c19df02e97
Viktoriia Svichkar <vitasvichkar@gmail.com>
2023-12-13 16:52:21 +0200

    dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let names = [];
+  for (let i = 0; i < members.length; i += 1) {
+    if(typeof members[i] === 'string')
+    names.push(members[i].trim().at(0).toUpperCase());
+  }
+  names = names.sort().join('');
+  if (typeof names === 'string')
+ return names;
 }
+createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']);
 
 module.exports = {
   createDreamTeam
